92c24525411c3d029db313b95bf0b87c704e1070
NatswarChuan <natswarchuan@gmail.com>
2025-08-12 08:55:56 +0700

    .

--- src/main/java/io/github/natswarchuan/vmc/core/config/VMCAutoConfiguration.java
+++ src/main/java/io/github/natswarchuan/vmc/core/config/VMCAutoConfiguration.java
@@ -3,14 +3,9 @@ package io.github.natswarchuan.vmc.core.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-/**
- * Lớp tự động cấu hình cho VMC Framework. Kích hoạt việc đăng ký VMC Repositories và quét các thành
- * phần cốt lõi.
- */
+/** Lớp tự động cấu hình cho VMC Framework. Quét các thành phần và mapper cốt lõi của framework. */
 @Configuration
-@Import(VMCRepositoryRegistrar.class)
 @ComponentScan("io.github.natswarchuan.vmc.core")
 @MapperScan("io.github.natswarchuan.vmc.core.persistence.mapper")
 public class VMCAutoConfiguration {}
