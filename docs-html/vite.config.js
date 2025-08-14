8ac9be90550a9451864ac76b5428c1178b8b2b53
NatswarChuan <natswarchuan@gmail.com>
2025-08-14 11:47:41 +0700

    .

--- docs-html/vite.config.js
+++ docs-html/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
 
 export default defineConfig({
+
   base: '/vmc-query-builder/',
   plugins: [vue()],
   build: {
-    outDir: 'docs'
+
+
+    outDir: resolve(__dirname, '../docs')
   }
 })
